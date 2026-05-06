37b7149cbd0d25900e7ee817bd6040c2539afeef
Frédéric Catellier-Poulin <frederic.catellier@cauca.ca>
2026-05-06 10:30:09 -0400

    Feat(): add log to the retry

--- Cauca.ApiClient/Services/RetryPolicyBuilder.cs
+++ Cauca.ApiClient/Services/RetryPolicyBuilder.cs
@@ -1,50 +1,52 @@
 ﻿using System;
-using System.Linq;
 using System.Net;
 using Cauca.ApiClient.Configuration;
 using Cauca.ApiClient.Extensions;
 using Flurl.Http;
 using Polly;
 
-namespace Cauca.ApiClient.Services
+namespace Cauca.ApiClient.Services;
+
+public class RetryPolicyBuilder : IRetryPolicyBuilder
 {
-    public class RetryPolicyBuilder : IRetryPolicyBuilder
+    public virtual IAsyncPolicy BuildRetryPolicy(int maxRetryAttemptOnTransientFailure)
     {
-        public virtual IAsyncPolicy BuildRetryPolicy(int maxRetryAttemptOnTransientFailure)
-        {
-            var retryPolicy = Policy
-               .Handle<FlurlHttpException>(IsTransientOrTimeOut)
-               .WaitAndRetryAsync(maxRetryAttemptOnTransientFailure, retryAttempt =>
-               {
-                   var nextAttemptIn = CalculateNextAttemptDelay(retryAttempt);
-                   Console.WriteLine($"Retry attempt {retryAttempt} to make request. Next try on {nextAttemptIn.TotalSeconds} seconds.");
-                   return nextAttemptIn;
-               });
+        var retryPolicy = Policy
+            .Handle<FlurlHttpException>(IsTransientOrTimeOut)
+            .WaitAndRetryAsync(
+                maxRetryAttemptOnTransientFailure,
+                CalculateNextAttemptDelay,
+                (exception, nextAttemptIn, retryAttempt, _) =>
+                {
+                    var flurlException = (FlurlHttpException)exception;
+                    var url = flurlException.Call?.Request?.Url?.ToString() ?? "unknown url";
+                    var statusCode = flurlException.StatusCode?.ToString() ?? "no response";
+                    Console.WriteLine($"Retry attempt {retryAttempt} for {url} returned {statusCode}. Next try in {nextAttemptIn.TotalSeconds} seconds.");
+                });
 
-            return retryPolicy;
-        }
+        return retryPolicy;
+    }
 
-        private bool IsTransientOrTimeOut(FlurlHttpException exception)
-        {
-            return IsTransientError(exception) || exception.Call.NoResponse();
-        }
+    private static bool IsTransientOrTimeOut(FlurlHttpException exception)
+    {
+        return IsTransientError(exception) || exception.Call.NoResponse();
+    }
 
-        private bool IsTransientError(FlurlHttpException exception)
-        {
-            int[] httpStatusCodesWorthRetrying =
-            {
-                (int)HttpStatusCode.RequestTimeout, // 408
-                (int)HttpStatusCode.BadGateway, // 502
-                (int)HttpStatusCode.ServiceUnavailable, // 503
-                (int)HttpStatusCode.GatewayTimeout // 504
-            };
+    private static bool IsTransientError(FlurlHttpException exception)
+    {
+        int[] httpStatusCodesWorthRetrying =
+        [
+            (int)HttpStatusCode.RequestTimeout, // 408
+            (int)HttpStatusCode.BadGateway, // 502
+            (int)HttpStatusCode.ServiceUnavailable, // 503
+            (int)HttpStatusCode.GatewayTimeout // 504
+        ];
 
-            return exception.StatusCode.HasValue && httpStatusCodesWorthRetrying.Contains(exception.StatusCode.Value);
-        }
+        return exception.StatusCode.HasValue && httpStatusCodesWorthRetrying.Contains(exception.StatusCode.Value);
+    }
 
-        protected virtual TimeSpan CalculateNextAttemptDelay(int retryAttempt)
-        {
-            return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
-        }
+    protected virtual TimeSpan CalculateNextAttemptDelay(int retryAttempt)
+    {
+        return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
     }
 }
